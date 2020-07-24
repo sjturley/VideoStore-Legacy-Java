68ff30fbe219230cc9df8aab21bd9634551b59c2
Steve Turley <steve@turleylabs.com>
2020-07-24 13:31:36 -0500

    Initial commit

---
+++ src/main/java/videostore/RentalService.java
@@ -0,0 +1,9 @@
+package videostore;
+
+import java.util.List;
+
+public class RentalService {
+    public static List<Rental> getRentals() {
+        throw new RuntimeException();
+    }
+}
---
+++ src/test/java/videostore/VideoStoreTest.java
@@ -0,0 +1,12 @@
+package videostore;
+
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.reporters.QuietReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.junit.Test;
+
+public class VideoStoreTest {
+    @Test
+    public void approveCustomerStatement() {
+    }
+}
