7f1b44e50abbecddaee04306c983dab5678558b4
Steve Turley <steve@turleylabs.com>
2020-07-24 16:14:36 -0500

    Parameterizing test logic

--- src/test/java/videostore/VideoStoreTest.java
+++ src/test/java/videostore/VideoStoreTest.java
@@ -1,28 +1,24 @@
 package videostore;
 
 import org.approvaltests.Approvals;
-import org.approvaltests.combinations.CombinationApprovals;
-import org.approvaltests.reporters.QuietReporter;
-import org.approvaltests.reporters.UseReporter;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class VideoStoreTest {
     @Test
     public void approveCustomerStatement() {
-        String statement = createStatement();
+        String statement = createStatement(Movie.REGULAR, 3);
 
         Approvals.verify(statement);
     }
 
-    private String createStatement() {
+    private String createStatement(int priceCode, int daysRented) {
         Customer customer = new Customer("Steve") {
             @Override
             protected List<Rental> getRentals() {
-                return Arrays.asList(new Rental(new Movie("Star Wars", Movie.REGULAR), 3));
+                return Arrays.asList(new Rental(new Movie("Star Wars", priceCode), daysRented));
             }
         };
         return customer.statement();
